226cc004131b96f381eb1157b7d6561925036004
Warrick FitzGerald <warrick.fitzgerald@gmail.com>
2022-03-03 15:37:13 -0600

    Added mock toChainEntityName function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import { SharedHelpers } from './helpers'
-let x = SharedHelpers.toChainEntityName('wozzawozza22')
+
+
+// This is all we need to export from the shared library
+export declare enum ChainEntityNameBrand {
+    _ = ""
+}
+export declare type ChainEntityName = string & ChainEntityNameBrand;
+function toChainEntityName2(value: any) : ChainEntityName {
+    return value as ChainEntityNameBrand;
+}
+
+
+// This is what we're currently doing 
+let x : ChainEntityName = SharedHelpers.toChainEntityName('wozzawozza22')
 console.log(x);
+
+let y : ChainEntityName = toChainEntityName2("wozzawozza22");
+console.log(y);
+
+// This will fail the compiler check
+//let z : ChainEntityName = "wozzawozza22";
+//console.log(z);
+
 let message: string = 'Hello, World!';
 console.log(message);
 
