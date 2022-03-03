7ee1c2b42d64682bfb5d6d7d20a5c36eaab323fa
Warrick FitzGerald <warrick.fitzgerald@gmail.com>
2022-03-03 14:40:42 -0600

    Webpack test added

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const path = require('path');
+const { env } = require('process');
 
-module.exports = {
-  entry: './src/index.ts',
+module.exports = (env, argv) => {
+
+  return {entry: './src/index.ts',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: [{
+            loader:'ts-loader',
+            options: {projectReferences: true}
+          }],
+        exclude: [
+          /node_modules/
+        ],        
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    fallback: {
+        "electron": false,
+    },
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]'
   },
+  devtool: 'cheap-module-source-map',
+  target: ["node"],
+  mode: 'development'
+}
 };
